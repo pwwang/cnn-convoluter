83ff8e6e5fe3a6629172df08fb27029ae674da66
pwwang <pwwang@pwwang.com>
2020-08-20 11:14:50 -0500

    Add visit counter

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,7 +2,15 @@
     <div class="logo text-white p-3 pl-5">
         <h5>CNN Convoluter</h5>
     </div>
-    <div class="social text-right pr-5 flex-grow">
+    <div class="text-right flex-grow text-white">Visits:</div>
+    <div class="pl-2 pr-5">
+        <!-- Start of WebFreeCounter Code -->
+        <a href="https://www.webfreecounter.com/" target="_blank">
+            <img src="https://www.webfreecounter.com/hit.php?id=gvfkdff&nd=6&style=3" border="0" alt="visitor counter" />
+        </a>
+        <!-- End of WebFreeCounter Code -->
+    </div>
+    <div class="social pr-8">
         <a href="https://github.com/pwwang/cnn-convoluter" target="_blank"
            class="inline-block">
             <img src="github.png" alt="Github" width="32" height="32" />
